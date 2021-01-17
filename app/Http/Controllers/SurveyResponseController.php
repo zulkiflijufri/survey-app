83299686d7054b0fcd65684169f8f8b7bdeb84e0
zulkiflijufri <zulkiflijufri54@gmail.com>
2021-01-17 20:33:43 +0800

    init model & controller

---
+++ app/Http/Controllers/SurveyResponseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SurveyResponse;
+use Illuminate\Http\Request;
+
+class SurveyResponseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SurveyResponse  $surveyResponse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SurveyResponse $surveyResponse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SurveyResponse  $surveyResponse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SurveyResponse $surveyResponse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SurveyResponse  $surveyResponse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SurveyResponse $surveyResponse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SurveyResponse  $surveyResponse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SurveyResponse $surveyResponse)
+    {
+        //
+    }
+}
